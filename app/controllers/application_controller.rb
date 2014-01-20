19bf6dc24d84a266182382d28f4e5facf7b52a4b
Arvind <=>
2014-01-20 16:55:58 +0500

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+	include UrlHelper
+  protect_from_forgery
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '7819d30b2b6e0c6fc2241b5ffe0645d0060667cb867f70597400f7b0fb1730535ea0d324c6ecde12b51e7a49c052fde631d38328e53ebc3efdfbbad8adbd6bb6'
